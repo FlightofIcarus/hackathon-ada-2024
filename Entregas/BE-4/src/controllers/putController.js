c7b0c60276c55fed8ae60b1f84fb335fa7dee37d
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-04-16 04:32:37 -0300

    style: change putController's variable name

--- Entregas/BE-4/src/controllers/putController.js
+++ Entregas/BE-4/src/controllers/putController.js
@@ -2,7 +2,7 @@ async function putController(req, res, routerService) {
     try {
         const requestItemId = await req.params.id;
         const requestBodyData = await req.body;
-        const puttResponse = await routerService(requestItemId, requestBodyData);
+        const putResponse = await routerService(requestItemId, requestBodyData);
         res.status(200).json({message: 'puts'})
     } catch (error) {
         res.status(500).json({message: 'errou!'})
