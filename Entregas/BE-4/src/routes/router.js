fc0c560664563f37865a5e5bc0bbf87e6a639e16
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-04-16 01:14:14 -0300

    feat/create set routers functions

--- Entregas/BE-4/src/routes/router.js
+++ Entregas/BE-4/src/routes/router.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const setGetRouters = require('../../config/routersFunctions/setGetRouter')
+const setPostRouters = require('../../config/routersFunctions/setPostRouter')
+const setPutRouters = require('../../config/routersFunctions/setPutRouter')
+const setDeleteRouters = require('../../config/routersFunctions/setDeleteRouter')
+const routerController = require('../controllers/controllers')
 
-router.get('/', (_, res) => {
-  return res.status(200).json({ message: 'Tudo ok' });
-})
+setGetRouters(router, '/', routerController)
+
+
+
+
+const a = (req, res) => {
+  return res.status(200).json({message: 'Rota get vagas.'})
+}
+
+setGetRouters(router, '/api/vagas', a)
+// router.get('/api/vagas', a)
 
 module.exports = router;
