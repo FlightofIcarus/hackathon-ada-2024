0ad10b2c2a196637d8633601efd2bd6749f1285b
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-04-16 06:16:33 -0300

    feat: basical strings validations

--- Entregas/BE-4/src/middlewares/validations/validationsFunctions/validateBasicStringInput.js
+++ Entregas/BE-4/src/middlewares/validations/validationsFunctions/validateBasicStringInput.js
@@ -1,3 +1,12 @@
-function validateBasicStringInput(input) {
-    
+const {isAStringType, isAEmptyString, isAFullSpaceString, isAStringGreaterThenTwoCharacters} = require('./stringValidations/stringValidations');
+
+async function validateBasicStringInput(inputString) {
+    if (!isAStringType(inputString) ||
+        isAEmptyString(inputString) ||
+        isAFullSpaceString(inputString) ||
+        !isAStringGreaterThenTwoCharacters(inputString)) {
+        throw new Error('Erro de validação do campo, favor revisar.');
+    }
+
+    return true;
 }
