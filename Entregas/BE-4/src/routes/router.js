d304df4aa4d531d475afcdb686a742f6ee5599eb
Murilo M Andrade <andrade.murilom@gmail.com>
2024-04-17 00:02:01 -0300

    fix: router endpoint fixed from user to candidatos

--- Entregas/BE-4/src/routes/router.js
+++ Entregas/BE-4/src/routes/router.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const employeeRoutes = require('./employee-routes')
+const candidateRoutes = require('./candidate-routes')
 
 const router = Router();
 
 function routes(app) {
     app.use('/api/funcionarios', employeeRoutes)
-    app.use('/api/users')
+    app.use('/api/candidatos', candidateRoutes)
 
 };
 
